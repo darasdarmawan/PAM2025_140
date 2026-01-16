5022735fd64da16ee60e0175c5c562ac0252d9be
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-17 04:54:05 +0700

    menambahkan fungsi getmoodlabel dan get mood color

--- app/src/main/java/com/example/moodcare2/utils/MoodUtils.kt
+++ app/src/main/java/com/example/moodcare2/utils/MoodUtils.kt
@@ -13,3 +13,24 @@ fun getMoodEmoji(moodLevel: String): String {
     }
 }
 
+fun getMoodLabel(moodLevel: String): String {
+    return when (moodLevel.lowercase()) {
+        "sangat baik" -> "Sangat Baik"
+        "baik" -> "Baik"
+        "netral" -> "Netral"
+        "buruk" -> "Buruk"
+        "sangat buruk" -> "Sangat Buruk"
+        else -> "Tidak Diketahui"
+    }
+}
+
+fun getMoodColor(moodLevel: String): Color {
+    return when (moodLevel.lowercase()) {
+        "sangat baik" -> Color(0xFFE91E63)
+        "baik" -> Color(0xFF8BC34A)
+        "netral" -> Color(0xFF00BCD4)
+        "buruk" -> Color(0xFFFF7700)
+        "sangat buruk" -> Color(0xFFF44336)
+        else -> Color(0xFFCDDC39)
+    }
+}
